8aae66dfa4fd438756a56f41586a9d111d2651c8
Md. Sumon Mia <sumon.ruetbd@gmail.com>
2023-09-08 22:10:42 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//text change
+function textChange(){
+    document.getElementById('h2').innerHTML="Hello World";
+}
+function textWelcome(){
+    document.getElementById('p').innerHTML="Welcome to JS";
+}
+function alertMessage(){
+    alert ('This is an alert message on window');
+}
+
+function happy(){
+    document.getElementById('h3').innerHTML="Hope You are Doing Well";
+}
+
+function end(){
+    document.getElementById('h4').innerHTML="TATA Bye Bye, See you soon";
+}
